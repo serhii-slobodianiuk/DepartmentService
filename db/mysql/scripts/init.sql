8ce6a7c485db9ccd6e792c981a1a9c9bb1d30189
serhii.slobodianiuk <serhii.slobodianiuk@aimprosoft.com>
2022-05-29 00:59:10 +0300

    Dockerized an Application

--- db/mysql/scripts/init.sql
+++ db/mysql/scripts/init.sql
@@ -31,12 +31,12 @@ values ('Marketing'),
 
 insert into employees(salary, dep_id, birthday, name, phone_number, e_mail)
 values (2000.00, 1, '2012-12-23', 'Alex', '3_8123456789', 'Alex@mail.com'),
-       (1500.00, 1, '2020-01-30', 'Anna', '4_5555775444', 'e_mailAna@mail.com'),
+       (1500.00, 1, '2020-01-30', 'Anna', '4_5555775444', 'e_mailAnna@mail.com'),
        (1700.00, 2, '2020-01-30', 'Tony', '193746599', 'e_mailTony@mail.com'),
        (1700.00, 1, '2020-01-30', 'Ivan', '49487836455', 'e_mailIvan@mail.com'),
        (1700.00, 3, '2020-01-30', 'Sarah', '544477666444', 'e_mailSarah@mail.com'),
-       (1700.00, 1, '2020-01-30', 'Tom', '948287263564', 'e_mailTom@mail.com'),
-       (1700.00, 2, '2020-01-30', 'John', '48579238275495', 'e_mailHanna@mail.com'),
-       (1700.00, 3, '2020-01-30', 'Tom', '934857219', 'e_mailAnna@mail.com'),
-       (1700.00, 2, '2020-01-30', 'Max', 'Finance', 'e_mailSara@mail.com'),
-       (1900.00, 3, '2020-01-30', 'Serhii', '6_4446668444', 'e_mailSerhii@mail.com');
+       (1700.00, 2, '2020-01-30', 'John', '48579238275495', 'e_mailJohn@mail.com'),
+       (1700.00, 3, '2020-01-30', 'Tom', '934857219', 'e_mailTom@gmail.com'),
+       (1700.00, 2, '2020-01-30', 'Max', 'Finance', 'e_mailMax@mail.com'),
+       (1900.00, 3, '2020-01-30', 'Serhii', '6_4446668444', 'e_mailSerhii@mail.com'),
+       (1900.00, 3, '1998-06-01', 'Daniil', '+30983431357', 'daniil_porvav@gmail.com');
